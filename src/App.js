517f77ed309de7465d200de7106283866b91f3bb
hiro <hmaeyama0510@gmail.com>
2021-05-27 05:59:50 +0900

    add css modules

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import "./styles.css";
 import { InlineStyle } from "./components/InlineStyle";
+import { CssModules } from "./components/CssModules";
 
 export default function App() {
-  return <InlineStyle />;
+  return (
+    <>
+      <InlineStyle />
+      <CssModules />
+    </>
+  );
 }
